d44281731307b4a22af29e83f578578e22e10484
chenbingcheng <bingcheng.chen@outlook.com>
2023-08-01 10:39:44 +0200

    DNA authen API

--- DNA Center/main.py
+++ DNA Center/main.py
@@ -1,65 +1,22 @@
 import requests
 from requests.auth import HTTPBasicAuth
-from dnac_config import DNAC_IP, DNAC_PORT, DNAC_USER, DNAC_PASSWORD
+import env_lab
 
-
-def get_device_list():
-    """
-    Building out function to retrieve list of devices. Using requests.get to make a call to the network device Endpoint
-    """
-    global token
-    token = get_auth_token() # Get Token
-    url = "https://sandboxdnac.cisco.com/api/v1/network-device"
-    hdr = {'x-auth-token': token, 'content-type' : 'application/json'}
-    resp = requests.get(url, headers=hdr)  # Make the Get Request
-    device_list = resp.json()
-    get_device_id(device_list)
-
-
-def get_device_id(device_json):
-    for device in device_json['response']: # Loop through Device List and Retrieve DeviceId
-        print("Fetching Interfaces for Device Id ----> {}".format(device['id']))
-        print('\n')
-        get_device_int(device['id'])
-        print('\n')
-
-
-def get_device_int(device_id):
-    """
-    Building out function to retrieve device interface. Using requests.get to make a call to the network device Endpoint
-    """
-    url = "https://sandboxdnac.cisco.com/api/v1/interface"
-    hdr = {'x-auth-token': token, 'content-type': 'application/json'}
-    querystring = {"macAddress": device_id} # Dynamically build the query params to get device specific Interface information
-    resp = requests.get(url, headers=hdr, params=querystring) # Make the Get Request
-    interface_info_json = resp.json()
-    print_interface_info(interface_info_json)
-
-
-def print_interface_info(interface_info):
-    print("{0:42}{1:17}{2:12}{3:18}{4:17}{5:10}{6:15}".
-          format("portName", "vlanId", "portMode", "portType", "duplex", "status", "lastUpdated"))
-    for int in interface_info['response']:
-        print("{0:42}{1:10}{2:12}{3:18}{4:17}{5:10}{6:15}".
-              format(str(int['portName']),
-                     str(int['vlanId']),
-                     str(int['portMode']),
-                     str(int['portType']),
-                     str(int['duplex']),
-                     str(int['status']),
-                     str(int['lastUpdated'])))
+requests.packages.urllib3.disable_warnings()
 
 
 def get_auth_token():
-    """
-    Building out Auth request. Using requests.post to make a call to the Auth Endpoint
-    """
-    url = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'       # Endpoint URL
-    resp = requests.post(url, auth=HTTPBasicAuth(DNAC_USER, DNAC_PASSWORD))  # Make the POST Request
-    token = resp.json()['Token']    # Retrieve the Token
-    return token    # Create a return statement for the Token
-
+    # Endpoint URL
+    endpoint = '/dna/system/api/v1/auth/token'
+    url = 'https://' + env_lab.DNA_CENTER['host'] + endpoint
+    # Make the POST Request
+    resp = requests.post(url, auth=HTTPBasicAuth(env_lab.DNA_CENTER['username'], env_lab.DNA_CENTER['password']), verify=False)
+    # Retrieve the Token from the returned JSON
+    token = resp.json()['Token']
+    # Print out the Token
+    print("Token Retrieved: {}".format(token))
+    # Create a return statement to send the token back for later use
+    return token
 
 if __name__ == "__main__":
-    get_device_int()
-
+    get_auth_token()
